58a7a6dc5a53f8cc6953cc79f43a6eab3a469349
huxidaer <1505903196@qq.com>
2023-04-13 23:25:47 +0800

    413
    Map

--- MOVE_AWSD/custom/custom_editor_model_preview_icon.py
+++ MOVE_AWSD/custom/custom_editor_model_preview_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134222459: {'path': 'UP1ModelPreview/134222459', 'key': 134222459, 'size': '180X100'}, 134265180: {'path': 'UP1ModelPreview/134265180', 'key': 134265180, 'size': '180X100'}}
